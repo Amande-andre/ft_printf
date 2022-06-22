5599bb3c7a38d753206ebdb238c3c6865fec506a
Andre MANDE <anmande@e3r11p2.42.fr>
2022-06-22 18:51:42 +0200

    good_to_go

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 18:43:16 by anmande           #+#    #+#             */
-/*   Updated: 2022/06/22 18:09:03 by anmande          ###   ########.fr       */
+/*   Updated: 2022/06/22 18:51:20 by anmande          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_printf(const char *s, ...)
 	va_list			args;
 	unsigned int	i;
 	unsigned int	len;
-	
+
 	va_start(args, s);
 	i = 0;
 	len = 0;
@@ -41,8 +41,11 @@ int	ft_printf(const char *s, ...)
 
 unsigned int	ft_process(va_list args, char s)
 {
+	char	*base;
+
+	base = "0123456789abcdef";
 	if (s == '%')
-		return(ft_putchar_fd('%', 1));
+		return (ft_putchar_fd('%', 1));
 	else if (s == 'c')
 		return (ft_putchar_fd(va_arg(args, int), 1));
 	else if (s == 's')
@@ -52,22 +55,10 @@ unsigned int	ft_process(va_list args, char s)
 	else if (s == 'x')
 		return (ft_flagx(va_arg(args, int), 1, "0123456789abcdef"));
 	else if (s == 'X')
-		return (ft_flagx(va_arg(args, int), 1, "0123456789ABCDEF"));	
+		return (ft_flagx(va_arg(args, int), 1, "0123456789ABCDEF"));
 	else if (s == 'p')
-		return (ft_flagp(va_arg(args, long unsigned int), 1, "0123456789abcdef"));
+		return (ft_flagp(va_arg(args, long unsigned int), 1, base));
 	else if (s == 'u')
 		return (ft_putnbr_u(va_arg(args, unsigned int), 1, 0));
 	return (0);
 }
-
-// int	main()
-// {
-// 	char 	*n = NULL;
-// 	char	*ptr = "truc";
-// 	printf("%d", printf("%c%d%%%%c%c%s%d%d%x%p%p%u%s%s", 'a', 42, 'b', ptr, 4242, -545, 20000242, n, ptr, -42424242, n, "ok"));
-// 	printf("\n");
-// 	printf("%d", ft_printf("%c%d%%%%c%c%s%d%d%x%p%p%u%s%s", 'a', 42, 'b', ptr, 4242, -545, 20000242, n, ptr, -42424242, n, "ok"));
-	
-// 	// printf("==%d", printf("%d", -123456789));
-// 	// printf("==%d", ft_printf("%d", -123456789));
-// }
